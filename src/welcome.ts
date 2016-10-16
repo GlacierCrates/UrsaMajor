83125c5adf4225c6b78ff65963111e698d10238a
ZoolWay <zoolway@gmail.com>
2016-10-16 16:43:40 +0200

    change: playing test sound after welcome alert

--- src/welcome.ts
+++ src/welcome.ts
@@ -1,16 +1,23 @@
-//import {computedFrom} from 'aurelia-framework';
+import {computedFrom} from 'aurelia-framework';
+
+import { Howl } from 'howler';
 
 export class Welcome {
-  heading = 'Welcome to the Aurelia Navigation App!';
+  public heading: string = 'Welcome to the Aurelia Navigation App!';
   firstName = 'John';
   lastName = 'Doe';
-  previousValue = this.fullName;
+  private previousValue: string = this.fullName;
+  private sound: Howl;
+
+  public activate() {
+    this.sound = new Howl({ src: 'assets/test.ogg' });
+  }
 
   //Getters can't be directly observed, so they must be dirty checked.
   //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
   //To optimize by declaring the properties that this getter is computed from, uncomment the line below
   //as well as the corresponding import above.
-  //@computedFrom('firstName', 'lastName')
+  @computedFrom('firstName', 'lastName')
   get fullName() {
     return `${this.firstName} ${this.lastName}`;
   }
@@ -18,6 +25,7 @@ export class Welcome {
   submit() {
     this.previousValue = this.fullName;
     alert(`Welcome, ${this.fullName}!`);
+    this.sound.play();
   }
 
   canDeactivate() {
